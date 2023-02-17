5ec73da776fe4eecd138c36ba9d33eaea8b4834e
franklinAnozie <cfanozie@gmail.com>
2023-02-17 22:51:14 +0100

    attempted advanced tasks

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - a function that prints lowercase alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number, secondNumber;
+for (number = '0'; number <= '9'; number++)
+{
+for (secondNumber = '0'; secondNumber <= '9'; secondNumber++)
+{
+if (number < secondNumber && number != secondNumber)
+{
+putchar(number);
+putchar(secondNumber);
+if (number != '8' || secondNumber <= '8')
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
