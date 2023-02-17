bf6a9d6b429b579d20aaa3c3dbec2bf50eff593b
franklinAnozie <cfanozie@gmail.com>
2023-02-17 04:10:24 +0100

    Fixed issues

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,4 +10,3 @@ int main(void)
 puts("\"Programming is like building a multiligual puzzle");
 return (0);
 }
-
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,4 +9,3 @@ int main(void)
 printf("%s", "with proper grammer, but the outcome is a piece of art, \n");
 return (0);
 }
-
