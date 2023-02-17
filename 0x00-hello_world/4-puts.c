5c50a19a6c78efbebb159ad1e45ddb96c1733868
franklinAnozie <cfanozie@gmail.com>
2023-02-17 04:15:48 +0100

    More Errors fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,10 @@
 /***
 ** main - Entry Point
 *** Return: Alway 0 (Success)
-***
 ***/
 
 int main(void)
 {
-puts("\"Programming is like building a multiligual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
