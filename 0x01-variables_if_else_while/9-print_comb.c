32d1a4642065f2ac4a025ec4257448f1da91f7b6
franklinAnozie <cfanozie@gmail.com>
2023-02-17 21:56:35 +0100

    fixed betty error

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - a function that prints lowercase alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number, combination;
+for (number = 0; number < 10; number++)
+{
+putchar(number);
+}
+putchar('\n');
+return (0);
+}
