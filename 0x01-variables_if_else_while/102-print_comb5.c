e6f7a9dc6f309f1e24e06cbaf781c5388c977a45
franklinAnozie <cfanozie@gmail.com>
2023-02-18 04:28:02 +0100

    fixed last task error

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,21 +11,23 @@ for (number = '0'; number <= '9'; number++)
 {
 for (secondNumber = '0'; secondNumber <= '9'; secondNumber++)
 {
-putchar(number);
-putchar(secondNumber);
-putchar(' ');
 for (thirdNumber = '0'; thirdNumber <= '9'; thirdNumber++)
 {
 if (secondNumber <= thirdNumber)
 {
-for (fourthNumber = '0'; fourthNumber <= '9'; fourthNumber)
+for (fourthNumber = '0'; fourthNumber <= '9'; fourthNumber++)
 {
-if (thirdNumber < fourthNumber)
+if ((thirdNumber < fourthNumber) || (thirdNumber > fourthNumber && fourthNumber <= '9'))
 {
+putchar(number);
+putchar(secondNumber);
+putchar(' ');
 putchar(thirdNumber);
 putchar(fourthNumber);
-if(number != '9' || secondNumber <= '8')
+if (number != '9' || secondNumber <= '8')
+{
 putchar(',');
+}
 putchar(' ');
 }
 }
