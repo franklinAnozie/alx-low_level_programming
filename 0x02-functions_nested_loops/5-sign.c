b1da2424913f7e0d39fe7198166089894ce791f6
franklinAnozie <cfanozie@gmail.com>
2023-02-22 00:52:26 +0100

    attempted few tasks

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,13 +9,15 @@ int print_sign(int n)
 char signs[] = "+-0";
 if (n > 0)
 {
-_putchar(signs[0]);
+_putchar (signs[0]);
 return (1);
-} else if (n == 0) {
-_putchar(signs[2]);
+} else if (n == 0)
+{
+_putchar (signs[2]);
 return (0);
-} else {
-_putchar(signs[1]);
+} else
+{
+_putchar (signs[1]);
 return(-1);
 }
 }
