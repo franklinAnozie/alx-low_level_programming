2712a44393abcc42a33dca23add7c36007c875af
franklinAnozie <cfanozie@gmail.com>
2023-02-22 00:56:01 +0100

    attempted few tasks

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,15 +11,15 @@ if (n > 0)
 {
 _putchar (signs[0]);
 return (1);
-} 
+}
 else if (n == 0)
 {
 _putchar (signs[2]);
 return (0);
-} 
+}
 else
 {
 _putchar (signs[1]);
-return( -1);
+return (-1);
 }
 }
