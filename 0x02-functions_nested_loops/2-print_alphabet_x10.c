38b6f8e5393a04ae8a5ee2a5d018e55b9585508f
franklinAnozie <cfanozie@gmail.com>
2023-02-22 00:37:34 +0100

    attempted few tasks

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print alphabet_x10 - prints all possible combinations of two two-digit numbers
+ * print_alphabet_x10 - prints
  * Return: 0 (Success)
  */
 void print_alphabet_x10(void)
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 int i;
 char alphabet;
 int numOfIterations = 10;
-for(i = 0; i < numOfIterations; i++)
+for( i = 0; i < numOfIterations; i++)
 {
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 _putchar(alphabet);
