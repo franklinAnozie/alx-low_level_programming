b12e7db38e5ce4aad9adad1f5fc43149c68650c6
franklinAnozie <cfanozie@gmail.com>
2023-02-22 01:05:37 +0100

    attempted few tasks

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include "main.h"
-
-int print_last_digit(int j){
-    int ld = j % 10;
-    if(ld < 0){
-        ld = -ld;
-    }
-    _putchar(ld + '0');
-    return (ld);
+/**
+ * print_last_digit - prints all possible combinations of two two-digit numbers
+ * @j: absolutr val
+ * Return: 0 (Success)
+ */
+int print_last_digit(int j)
+{
+int ld = j % 10;
+if (ld < 0)
+ld = -ld;
+_putchar(ld + '0');
+return (ld);
 }
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
-void jack_bauer(void){
-    int i, j, k, l;
-    for(i=0; i <= 2; i++){
-        for(j=0; j <= 3; j++){
-            for (k=0; k <= 5; k++){
-                for (l=0; l <= 9; l++){
-                    printf("%d%d:%d%d\n",i,j,k,l);
-                }
-
-            }
-            
-        }
-    }
-    return;
+/**
+ * jack_bauer - prints all possible combinations of two two-digit numbers
+ * 
+ * Return: 0 (Success)
+ */
+void jack_bauer(void)
+{
+int i, j, k, l;
+for(i=0; i <= 2; i++)
+{
+for(j=0; j <= 3; j++)
+{
+for (k=0; k <= 5; k++)
+{
+for (l=0; l <= 9; l++)
+{
+printf("%d%d:%d%d\n",i,j,k,l);
+}
+}
+}
+}
+return;
 }
