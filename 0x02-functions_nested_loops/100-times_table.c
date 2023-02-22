689f09d05da780009028e947168f86eabbcd764d
franklinAnozie <cfanozie@gmail.com>
2023-02-22 02:27:45 +0100

    attempted first advanced task

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * print_to_98 - prints all possible combinations of two two-digit numbers
+ * @n: condition number
+ * Return: 0 (Success)
+ */
+void print_times_table(int n)
+{
+if ((n > 0 && n < 16) || n == 0)
+{
+int i, j, k;
+for (i = 0; i < n+1; i++)
+{
+for (j = 0; j < n+1; j++)
+{
+k = i * j;
+if (j < n)
+{
+printf("%3d, ", k);
+}
+else
+{
+printf("%3d", k);
+}
+}
+printf("\n");
+}
+}
+}
