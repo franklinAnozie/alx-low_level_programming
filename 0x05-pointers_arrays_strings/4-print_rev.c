7bb5d7991be6afe23195443efe85a319340a9be6
franklinAnozie <cfanozie@gmail.com>
2023-02-28 01:38:29 +0100

    fixed minor errors

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,19 +7,21 @@
 
 void print_rev(char *s)
 {
-int i = 0;
+int i, j, len;
+
+i = 0;
 
 while (s[i] != '\0')
 {
 i++;
 }
-int j = i;
-while (j>=0)
+
+len = i;
+
+for (j = len - 1; j >= 0; j--)
 {
 _putchar(s[j]);
-j--;
 }
 
 _putchar('\n');
-
 }
