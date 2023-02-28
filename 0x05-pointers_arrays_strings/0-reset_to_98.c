c28774b5a0ba21b0aa23f144fc7762a87fc3e470
franklinAnozie <cfanozie@gmail.com>
2023-02-28 01:29:01 +0100

    Attempted all tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to 98
+ * @n: pointer to the variable to update
+ */
+
+void reset_to_98(int *n)
+{
+int *p = n;
+*p = 98;
+}
