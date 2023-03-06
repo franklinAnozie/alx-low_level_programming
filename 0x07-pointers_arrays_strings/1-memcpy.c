2d34394bc1d0e6893297eb35202f293147e837ed
franklinAnozie <cfanozie@gmail.com>
2023-03-07 00:49:52 +0100

    Attempted memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+/**
+ * _memcpy - prints buffer in hexa
+ * @dest: the address of memory to print
+ * @src: the size of the memory to print
+ * @n: the number of bytes to print
+ * Return: Nothing.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int i;
+
+    for (i = 0; i < n; i++)
+    {
+        dest[i] = src[i];
+    }
+
+    return (dest);
+}
