8bbdcdba053dc07ab29d6fb9059f6e0558d17b91
franklinAnozie <cfanozie@gmail.com>
2023-03-07 00:40:35 +0100

    attempted first task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+/**
+ * _memset - prints buffer in hexa
+ * @s: the address of memory to print
+ * @b: the size of the memory to print
+ * @n: the number of bytes to print
+ *
+ * Return: pointer to s.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+
+return (s);
+}
