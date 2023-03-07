1a616bd45b925b5df4d48d988a613569e1d9479e
franklinAnozie <cfanozie@gmail.com>
2023-03-07 02:48:07 +0100

    attempted more tasks

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,31 +1,18 @@
+#include "main.h"
 /**
- * _strchr - prints buffer in hexa
- * @s: the address of memory to print
- * @c: the size of the memory to print
- *
- * Return: Nothing.
+ * _strchr - locates a character in a string,
+ * @s: string.
+ * @c: character.
+ * Return: the pointer to the first occurrence of the character c.
  */
-
 char *_strchr(char *s, char c)
 {
-int i, j;
+unsigned int i = 0;
 
-for (i = 0; s[i] != '\0'; )
-{
-i++;
-}
-
-for (j = 0; j < i; j++)
-{
-if (s[j] == c)
-{
-break;
-}
-if (s[j] != c && s[j] == '\0')
-{
-break;
-}
-}
-
-return (&s[j]);
+for (; *(s + i) != '\0'; i++)
+if (*(s + i) == c)
+return (s + i);
+if (*(s + i) == c)
+return (s + i);
+return ('\0');
 }
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,27 +1,28 @@
-/**
- * _strspn - prints buffer in hexa
- * @s: the address of memory to print
- * @accept: the size of the memory to print
- *
- * Return: Nothing.
- */
+#include "main.h"
 
+/**
+ * _strspn - gets the length of a prefix substring.
+ * @s: initial segment.
+ * @accept: accepted bytes.
+ * Return: the number of accepted bytes.
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-int i, j;
-unsigned int count = 0;
+unsigned int i, j, bool;
 
-for (i = 0; s[i] != '\0'; )
+for (i = 0; *(s + i) != '\0'; i++)
 {
-for (j = 0; j < i; j++)
+bool = 1;
+for (j = 0; *(accept + j) != '\0'; j++)
 {
-if (s[i] == accept[j])
+if (*(s + i) == *(accept + j))
 {
-count++;
+bool = 0;
+break;
 }
 }
-i++;
+if (bool == 1)
+break;
 }
-
-return (count);
+return (i);
 }
