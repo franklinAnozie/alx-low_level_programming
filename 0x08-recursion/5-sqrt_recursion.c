dcf08e93aade94232d3270572eb84b0e87e7e5b2
franklinAnozie <cfanozie@gmail.com>
2023-03-08 19:35:06 +0100

    attempted mandatory tasks

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+int _sqrt(int x, int y);
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
+ *
+ * Return: the resulting square root
+ */
+
+int _sqrt_recursion(int n)
+{
+return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt_ - recurses to find the natural
+ * square root of a number
+ * @x: number to calculate the sqaure root of
+ * @y: iterator
+ *
+ * Return: the resulting square root
+ */
+
+int _sqrt(int x, int y)
+{
+int z;
+z = x - y;
+if (z == 0)
+{
+return (1);
+}
+if (z < 0)
+{
+return (-1);
+}
+return (1 + _sqrt(z, x + 2));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,42 @@
+int prime(int n, int i);
+
+/**
+ * is_prime_number - says if an integer is a prime number or not
+ * @n: number to evaluate
+ *
+ * Return: 1 if n is a prime number, 0 if not
+ */
+
+int is_prime_number(int n)
+{
+if (n < 0)
+{
+return (0);
+}
+return (prime(n, n-1));
+}
+
+/**
+ * prime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+
+int prime(int n, int  i)
+{
+if (i == 1)
+{
+return (1);
+}
+if (i == 0)
+{
+return (0);
+}
+if (n % i == 0)
+{
+return (0);
+}
+return (prime(n, i-1));
+}
