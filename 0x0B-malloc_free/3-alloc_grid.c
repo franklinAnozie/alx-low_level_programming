9da657a8b1641374c331da98fa1c6afacab1a7cb
franklinAnozie <cfanozie@gmail.com>
2023-03-14 20:43:25 +0100

    fixed alloc grid issues

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -30,16 +30,17 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int *)malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
 			for (; i >= 0;)
 			{
 				free(grid[i]);
+				i--;
 			}
 			free(grid);
 			return (NULL);
 		}
+		grid[i] = (int *)malloc(sizeof(int) * width);
 	}
 
 	for (j = 0; j < height; j++)
