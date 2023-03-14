ee0eeb750524ce0f6fa54c67ee5f966500b7ab43
franklinAnozie <cfanozie@gmail.com>
2023-03-14 20:37:41 +0100

    betty issues

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,24 +15,16 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k, l;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	for (i = 0; s1[i] != '\0';)
-	{
 		i++;
-	}
 
 	for (j = 0; s2[j] != '\0';)
-	{
 		j++;
-	}
 
 	new_str = malloc(sizeof(char) * (i + j + 1));
 
