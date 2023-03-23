01267ebc12c4ec4354730da2b401976e0c704c69
franklinAnozie <cfanozie@gmail.com>
2023-03-23 03:52:23 +0100

    fixed more typos

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "3-calc.h"
 
 /**
@@ -20,9 +21,8 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	if (*argv[2] != '/' && *argv[2] != '%'
-	&& *argv[2] != '*' && *argv[2] != '-'
-	&& *argv[2] != '+' || strlen(*argv[2] != 1))
+	if ((*argv[2] != '/' && *argv[2] != '%' && *argv[2] != '*' && *argv[2] != '-'
+	&& *argv[2] != '+') || strlen(argv[2]) != 1)
 	{
 		printf("Error\n");
 		exit(98);
