b9451e2ddbec3b8ad2cbc64a3bf7c80a2acf8038
franklinAnozie <cfanozie@gmail.com>
2023-04-11 03:21:40 +0100

    correction file i/o

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,17 +16,12 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (!filename && !text_content)
+	if (!text_content)
 	{
 		return (-1);
 	}
 
-	if (filename && !text_content)
-	{
-		return (-1);
-	}
-
-	file = open(filename, O_APPEND | O_WRONLY);
+	file = open(filename, O_WRONLY | O_APPEND);
 
 	if (file == -1)
 	{
