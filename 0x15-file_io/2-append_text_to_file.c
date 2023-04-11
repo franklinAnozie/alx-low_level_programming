61f5fc734c2768a56768f054f2ecdf96750ab149
franklinAnozie <cfanozie@gmail.com>
2023-04-11 19:04:17 +0100

    fixed append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,9 +16,9 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (!text_content)
+	if (filename && !text_content)
 	{
-		return (-1);
+		return (1);
 	}
 
 	file = open(filename, O_WRONLY | O_APPEND);
