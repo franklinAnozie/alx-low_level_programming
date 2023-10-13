a40c90d5567d09faba21404598a8730fc35da410
franklinAnozie <cfanozie@gmail.com>
2023-10-13 01:12:26 +0100

    fixed betty issues

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		return (NULL);
 
 	point = head;
-	while (point->next != NULL)
+	while (point != NULL)
 	{
 		if (i == index)
 			return (point);
