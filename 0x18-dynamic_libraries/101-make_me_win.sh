71070c740f9105867e0b83938a9276735b68d4ca
franklinAnozie <cfanozie@gmail.com>
2023-10-20 22:35:32 +0100

    fixed changes

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qO /tmp/libwin.so https://github.com/franklinAnozie/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libwin.so
+wget -qO /tmp/libwin.so https://github.com/franklinAnozie/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libwin.so
 export LD_PRELOAD=/tmp/libwin.so
