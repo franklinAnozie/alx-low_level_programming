cdc45e0f1009f58bc0e9d6acd85f47230be5cb45
franklinAnozie <cfanozie@gmail.com>
2024-03-12 16:32:38 +0100

    jump error

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,7 +12,7 @@
 int jump_search(int *array, size_t size, int value)
 {
 	int n_size = (int) size;
-	int start, end = 0;
+	int start = 0, end = 0;
 	int checker = 0;
 
 	if (array == NULL)
@@ -23,7 +23,7 @@ int jump_search(int *array, size_t size, int value)
 		printf("Value checked array[%d] = [%d]\n", end, array[end]);
 		start = end;
 		end = end + (int) sqrt(n_size);
-		if (end > n_size)
+		if (end >= n_size)
 			break;
 	}
 
