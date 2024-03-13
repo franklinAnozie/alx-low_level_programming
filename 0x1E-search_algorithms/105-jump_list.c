17506cc045fc6d416f93f18b0ea84e8162375339
franklinAnozie <cfanozie@gmail.com>
2024-03-13 07:12:48 +0100

    jump_list

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,45 @@
+#include "search_algos.h"
+
+/**
+ * jump_list - Jump Search Algo
+ * @list: list to search in
+ * @size: size of array
+ * @value: value to search for
+ *
+ * Return: index where value is found or -1
+ */
+
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	listint_t *head, *tail;
+	int sz = (int) size, jump;
+
+	if (list == NULL)
+		return (NULL);
+
+	for (head = tail = list; head->index < size; head = tail)
+	{
+		jump = (int) sqrt(sz);
+		while (jump)
+		{
+			if (tail->next == NULL)
+				break;
+			tail = tail->next;
+			jump--;
+		}
+		printf("Value checked at index [%ld] = [%d]\n", tail->index, tail->n);
+		if (tail->n > value || tail->next == NULL)
+			break;
+	}
+	printf("Value found between index [%ld] and [%ld]\n",
+			head->index, tail->index);
+	while (head && head != tail)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", head->index, head->n);
+		if (head->n == value)
+			return (head);
+		head = head->next;
+	}
+
+	return (NULL);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,22 @@
 #include <stdlib.h>
 #include <math.h>
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
@@ -13,5 +29,6 @@ int exponential_search(int *array, size_t size, int value);
 int bs(int *array, int start, int end, int value);
 int advanced_binary(int *array, size_t size, int value);
 int a_bs(int *array, int start, int end, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif
