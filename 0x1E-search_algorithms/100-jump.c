b48c5638361871987c676b1ed79553255f0338ca
franklinAnozie <cfanozie@gmail.com>
2024-03-13 07:45:43 +0100

    jump fix

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -11,39 +11,37 @@
 
 int jump_search(int *array, size_t size, int value)
 {
-	int n_size = (int) size;
-	int start = 0, end = 0;
+	int n_size = (int) size, iter;
+	int start = 0, end;
 	int checker = 0;
 
 	if (array == NULL)
 		return (-1);
 
-	while (array[end] < value)
+	for (end = 0; end < n_size; start = end)
 	{
 		printf("Value checked array[%d] = [%d]\n", end, array[end]);
-		start = end;
 		end = end + (int) sqrt(n_size);
+		if (array[end] >= value)
+			break;
 		if (end >= n_size)
 			break;
 	}
-
-	while (array[start] < value)
+	if (end > n_size)
+		iter = n_size - start;
+	else
+		iter = end - start;
+	while (iter)
 	{
 		if (checker == 0)
 			printf("Value found between indexes [%d] and [%d]\n", start, end);
 
 		printf("Value checked array[%d] = [%d]\n", start, array[start]);
+		if (array[start] == value)
+			return (start);
 		start++;
 		checker = 1;
-		if (start > end || end >= n_size)
-			return (-1);
+		iter--;
 	}
-
-	if (array[start] == value)
-	{
-		printf("Value checked array[%d] = [%d]\n", start, array[start]);
-		return (start);
-	}
-	else
-		return (-1);
+	return (-1);
 }
